7d5f99937da72dc4f186af3ac612330e29df6779
jacoblovins <lovins.jacob@yahoo.com>
2020-07-23 21:00:19 -0400

    started face recignition

--- script.js
+++ script.js
@@ -139,4 +139,36 @@ function GetSelectedTextValue() {
     };
     listenForSpeech();
 };
+
+$(".modalBtn").click(function(){
+    videoSetUp()
+})
+
+function videoSetUp() {
+    Webcam.set({
+        width: 320,
+        height: 240,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+    });
+    Webcam.attach('#my_camera');
+
+    $(".takeSnapshot").click(function(){
+        take_snapshot();
+    })
+
+    // <!Code to handle taking the snapshot and displaying it locally 
+    function take_snapshot() {
+
+        // take snapshot and get image data
+        Webcam.snap(function (data_uri) {
+            // display results in page
+            console.log(data_uri);
+            document.getElementById('results').innerHTML =
+                '<img src="' + data_uri + '"/>';
+        });
+    }
+}
+
+
 GetSelectedTextValue();
